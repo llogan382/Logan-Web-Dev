1023deb3a4fbd002cefd30d0433bf7b2a02efa67
Luke Logan <llogan382@gmail.com>
2019-01-25 09:44:48 -0500

    initial files

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package loganwebdev
+ */
+
+?>
+
+	</div><!-- #content -->
+	<div class="fixed-bottom lwd-footer">
+
+
+
+		<a href="mailto:luke@loganwebdev.com?subject=From%The%20Website">Email Logan Web Dev</a>
+
+
+
+	</div>
+
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ page.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package loganwebdev
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<div class="container front-page-container">
+	<div class="row">
+		<div class="col">
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+		</div>
+	</div>		
+
+
+</div>
+
+
+
+
+<?php
+get_sidebar();
+get_footer();
